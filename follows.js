7daed4788b727cbd7572f64bf0eef51299a1f10a
heinebell <heinebell@yahoo.com>
2014-09-28 18:45:54 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,11 +12,11 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '257HV6ylvicYyFmN2HK_CZFO1R2SAKxlbmdLc25nQ3NROD0g', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [124387], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Tollies Stellenbosch', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 100, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
